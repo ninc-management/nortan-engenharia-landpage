b307f08979ac223514ccb8768f5da145e6431b67
Augusto Icaro Farias da Cunha <acunha@tecgraf.puc-rio.br>
2022-01-12 16:59:13 -0800

    Improve team page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,12 +2,12 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-$(document).ready(function() {
+$(document).ready(function () {
   // FitVids init
   $("#main").fitVids();
 
   // Sticky sidebar
-  var stickySideBar = function() {
+  var stickySideBar = function () {
     var show =
       $(".author__urls-wrapper button").length === 0
         ? $(window).width() > 1024 // width should match $large Sass variable
@@ -23,18 +23,18 @@ $(document).ready(function() {
 
   stickySideBar();
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     stickySideBar();
   });
 
   // Follow menu drop down
-  $(".author__urls-wrapper button").on("click", function() {
+  $(".author__urls-wrapper button").on("click", function () {
     $(".author__urls").toggleClass("is--visible");
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
   // Close search screen with Esc key
-  $(document).keyup(function(e) {
+  $(document).keyup(function (e) {
     if (e.keyCode === 27) {
       if ($(".initial-content").hasClass("is--hidden")) {
         $(".search-content").toggleClass("is--visible");
@@ -44,11 +44,11 @@ $(document).ready(function() {
   });
 
   // Search toggle
-  $(".search__toggle").on("click", function() {
+  $(".search__toggle").on("click", function () {
     $(".search-content").toggleClass("is--visible");
     $(".initial-content").toggleClass("is--hidden");
     // set focus on input
-    setTimeout(function() {
+    setTimeout(function () {
       $(".search-content input").focus();
     }, 400);
   });
@@ -58,11 +58,11 @@ $(document).ready(function() {
     offset: 20,
     speed: 400,
     speedAsDuration: true,
-    durationMax: 500
+    durationMax: 500,
   });
 
   // Gumshoe scroll spy init
-  if($("nav.toc").length > 0) {
+  if ($("nav.toc").length > 0) {
     var spy = new Gumshoe("nav.toc a", {
       // Active classes
       navClass: "active", // applied to the nav list item
@@ -77,7 +77,7 @@ $(document).ready(function() {
       reflow: true, // if true, listen for reflows
 
       // Event support
-      events: true // if true, emit custom events
+      events: true, // if true, emit custom events
     });
   }
 
@@ -99,43 +99,158 @@ $(document).ready(function() {
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
     },
     image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
     removalDelay: 500, // Delay in milliseconds before popup is removed
     // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: "mfp-zoom-in",
     callbacks: {
-      beforeOpen: function() {
+      beforeOpen: function () {
         // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace(
           "mfp-figure",
           "mfp-figure mfp-with-anim"
         );
-      }
+      },
     },
     closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
   // Add anchors for headings
-  $('.page__content').find('h1, h2, h3, h4, h5, h6').each(function() {
-    var id = $(this).attr('id');
-    if (id) {
-      var anchor = document.createElement("a");
-      anchor.className = 'header-link';
-      anchor.href = '#' + id;
-      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>';
-      anchor.title = "Permalink";
-      $(this).append(anchor);
+  $(".page__content")
+    .find("h1, h2, h3, h4, h5, h6")
+    .each(function () {
+      var id = $(this).attr("id");
+      if (id) {
+        var anchor = document.createElement("a");
+        anchor.className = "header-link";
+        anchor.href = "#" + id;
+        anchor.innerHTML =
+          '<span class="sr-only">Permalink</span><i class="fas fa-link"></i>';
+        anchor.title = "Permalink";
+        $(this).append(anchor);
+      }
+    });
+
+  nameSort = function (direction = 1, a, b) {
+    return a
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "") <
+      b
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+      ? -1 * direction
+      : direction;
+  };
+
+  // get plataform users
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "POST",
+    "https://plataforma.nortanengenharia.com/api/public/user/all",
+    true
+  );
+  xhr.send(JSON.stringify({}));
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState != 4) return;
+
+    if (this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      data = data.sort((a, b) => {
+        return nameSort(1, a.fullName, b.fullName);
+      });
+      data.splice(
+        data.findIndex(
+          (member) => member.fullName == "Augusto Ícaro Farias da Cunha"
+        ),
+        1
+      );
+      const augusto = data.splice(
+        data.findIndex(
+          (member) => member.fullName == "José Augusto Gomes da Cunha"
+        ),
+        1
+      );
+      data.unshift(augusto[0]);
+      const jessica = data.splice(
+        data.findIndex((member) => member.fullName == "Jéssica Gonçalves"),
+        1
+      );
+      data.unshift(jessica[0]);
+      const hugo = data.splice(
+        data.findIndex(
+          (member) => member.fullName == "Augusto Hugo Farias da Cunha"
+        ),
+        1
+      );
+      data.unshift(hugo[0]);
+      var wrapperDiv = '<div class="feature__wrapper">';
+
+      for (const member of data) {
+        var excerpt = "";
+        switch (member.fullName) {
+          case "Augusto Hugo Farias da Cunha":
+            excerpt =
+              "Diretor Executivo - CEO<br/>" + member.mainDepartment.slice(6);
+            break;
+          case "Jéssica Gonçalves":
+            excerpt =
+              "Diretora de Marketing<br/>" + member.mainDepartment.slice(6);
+            break;
+          case "Tales Augusto Costa Gomes":
+            excerpt =
+              "Diretor de Operações<br/>" + member.mainDepartment.slice(6);
+            break;
+          default:
+            excerpt = member.mainDepartment.slice(6);
+            break;
+        }
+        const el =
+          `<div class="feature__item--team"><div class="archive__item"><div class="archive__item-teaser"><img src="` +
+          member.profilePicture +
+          `" alt="` +
+          member.fullName +
+          `" style="" /></div><div class="archive__item-body"><h2 class="archive__item-title">` +
+          (member.exibitionName ? member.exibitionName : member.fullName) +
+          `</h2><div class="archive__item-excerpt"><p><strong>` +
+          excerpt +
+          `</strong></p></div></div></div></div>`;
+        wrapperDiv += el;
+      }
+      wrapperDiv += "</div>";
+
+      // we get the returned data
+      var sliderData = document.querySelector("#slider3");
+      if (sliderData) {
+        sliderData.insertAdjacentHTML("afterbegin", wrapperDiv);
+      }
+
+      var slider = new IdealHTMLSlider.Slider({
+        selector: "#slider3",
+        height: "auto",
+        initialHeight: 300,
+        maxHeight: 500,
+        interval: 3000,
+        group: 4,
+        groupClasses: "feature__wrapper",
+        hasBorder: false,
+      });
+      slider.addBulletNav();
+      slider.start();
     }
-  });
+
+    // end of state change: it can be after some time (async)
+  };
 
   // fix nav height
   var el = document.querySelector(".nav-tabs");
-  if (el)
-    el.style.height = getComputedStyle(el).height;
+  if (el) el.style.height = getComputedStyle(el).height;
 });
